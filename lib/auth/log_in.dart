7199124d724d34f9f02d9e4e30612499c9c770b7
NadiaIslamRupa <nadiaislamrupa853@gmail.com>
2024-08-02 11:52:11 +0600

    update log in page

--- lib/auth/log_in.dart
+++ lib/auth/log_in.dart
@@ -7,6 +7,67 @@ class LogIn extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'Enter your name or mail: ',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            TextField(
+              // controller: nameController,
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.blue[100],
+                border: OutlineInputBorder(
+                  borderSide: BorderSide.none,
+                  borderRadius: BorderRadius.circular(25),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              'Enter your password: ',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            TextField(
+              //controller: passController,
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.blue[100],
+                border: OutlineInputBorder(
+                  borderSide: BorderSide.none,
+                  borderRadius: BorderRadius.circular(25),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text(
+                  'Log In',
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
