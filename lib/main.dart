c80d198be1397dd9583db28cba41cf6e94184201
NadiaIslamRupa <nadiaislamrupa853@gmail.com>
2024-08-02 11:41:08 +0600

    add log in page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:finance_app/ui/sign_in.dart';
+import 'package:finance_app/auth/sign_in.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
