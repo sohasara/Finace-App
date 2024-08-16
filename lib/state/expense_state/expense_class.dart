d0bf5631d0d9a88e5629f238a98e04f556f2493e
Nadia Islam <sohasara853@gmail.com>
2024-08-16 18:50:24 +0600

    update data class

--- lib/state/expense_state/expense_class.dart
+++ lib/state/expense_state/expense_class.dart
@@ -5,35 +5,6 @@ import 'package:finance_app/database/expense_model/expense_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
-// class ExpenseNotifier extends StateNotifier<List<ExpenseModel>> {
-//   final Box<ExpenseModel>? _box;
-//   ExpenseNotifier(this._box) : super([]) {
-//     loadExpenses();
-//   }
-
-//   void loadExpenses() {
-//     if (_box != null) {
-//       state = _box.values.toList();
-//     }
-//   }
-
-//   void addExpense(
-//       double amount, String category, BuildContext context, String time) {
-//     if (_box == null) return;
-
-//     final expense =
-//         ExpenseModel(amount: amount, category: category, time: time);
-//     _box.add(expense);
-//     state = [...state, expense];
-//   }
-
-//   void removeExpense(int index) {
-//     if (_box == null) return;
-
-//     _box.deleteAt(index);
-//     state = List.from(state)..removeAt(index);
-//   }
-// }
 class ExpenseNotifier extends StateNotifier<List<ExpenseModel>> {
   final Box<ExpenseModel>? _box;
 
@@ -43,7 +14,7 @@ class ExpenseNotifier extends StateNotifier<List<ExpenseModel>> {
 
   void loadExpenses() {
     if (_box != null) {
-      state = _box!.values.toList();
+      state = _box.values.toList();
     }
   }
 
