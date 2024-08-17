826523d65e9174b3fc28c7ebc7af9c2d3fd2b925
Nadia Islam <sohasara853@gmail.com>
2024-08-17 20:19:44 +0600

    add etailes page

---
+++ lib/ui/income_page/income_detailes.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DetailesIncome extends StatelessWidget {
+  const DetailesIncome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
