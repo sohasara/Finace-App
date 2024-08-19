1f636a8c3b9c1f2e2ab94ea48ec3f6d987fb21d9
Nadia Islam <sohasara853@gmail.com>
2024-08-19 06:58:33 +0600

    change background color

--- lib/ui/income_page/income_detailes.dart
+++ lib/ui/income_page/income_detailes.dart
@@ -17,25 +17,44 @@ class DetailesIncome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Detailes Page',
-          style: TextStyle(
-            color: Colors.green,
-            fontWeight: FontWeight.w700,
-            fontSize: 25,
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Colors.pink[200]!,
+              Colors.green[200]!,
+              //Colors.blue[200]!,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
           ),
         ),
-        centerTitle: true,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
-            Details(text1: 'Amount:', text2: amount),
-            Details(text1: 'Category:', text2: cat),
-            Details(text1: 'Time:', text2: time),
-            Details(text1: 'Description:', text2: des!),
+            AppBar(
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              title: Text(
+                'Detailes Page',
+                style: TextStyle(
+                  color: Colors.green[700],
+                  fontWeight: FontWeight.w700,
+                  fontSize: 25,
+                ),
+              ),
+              centerTitle: true,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Details(text1: 'Amount:', text2: amount),
+                  Details(text1: 'Category:', text2: cat),
+                  Details(text1: 'Time:', text2: time),
+                  Details(text1: 'Description:', text2: des!),
+                ],
+              ),
+            ),
           ],
         ),
       ),
