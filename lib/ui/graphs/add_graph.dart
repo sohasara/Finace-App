cdb5804db3b32ead1e2e250f3b7ca14e37d0758e
Nadia Islam <sohasara853@gmail.com>
2024-08-23 17:04:48 +0600

    add pie Chart

--- lib/ui/graphs/add_graph.dart
+++ lib/ui/graphs/add_graph.dart
@@ -1,1 +1,17 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/widgets.dart';
 
+class AddGraph extends StatelessWidget {
+  const AddGraph({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return PieChart(
+      PieChartData(
+        sections: [
+          PieChartSectionData(),
+        ],
+      ),
+    );
+  }
+}
